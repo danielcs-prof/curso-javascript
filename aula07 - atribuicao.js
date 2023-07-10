4e7699812c668836346be84ac865738b7c2235f8
daniel-prof <danielcs80@gmail.com>
2023-07-10 00:13:27 -0300

    Atribuição

---
+++ aula07 - atribuicao.js
@@ -0,0 +1,17 @@
+// ATRIBUICOES 
+
+// 1 - Atribuicoes a variaveis
+let a = 1
+let x=1 , y=2;
+let b = c = 1
+console.log(a, b, c)
+
+// 2 - Incrementos e Acumuladores
+a++
+++b
+console.log(a,b)
+a += 2
+b -= 2
+//b =* 2
+//c =/ 2
+console.log(a,b)
